c748b1e3ff620ae80c221275b7d0fb8640eea2b7
yeagin <yeagin09@gmail.com>
2020-08-26 02:44:59 +0900

    first commit

--- app/src/main/java/com/example/igo/Add_Person.java
+++ app/src/main/java/com/example/igo/Add_Person.java
@@ -31,7 +31,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 
-
+//hello
 public class Add_Person extends AppCompatActivity {
     EditText enter_name;
     EditText enter_address;
