73f731446400fc429922121a0ac8155a5c742f26
JungHyun <f7817455@naver.com>
2020-09-03 22:09:53 +0900

    logout 구현

--- app/src/main/java/com/example/igo/login/SplashActivity.java
+++ app/src/main/java/com/example/igo/login/SplashActivity.java
@@ -18,19 +18,19 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-
+        /*
         //기존에 로그인이 되어있지않다면
         if (user == null) {
             myStartActivity(LoginActivity.class);
         } else {
             myStartActivity(MainActivity.class);
         }
+        */
+        myStartActivity(LoginActivity.class);
     }
 
     private void myStartActivity(Class c) {
         Intent intent = new Intent(this, c);
-        //main 액티비티로 들어간 후 뒤로가기 시 앱 종료
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/igo/main/Frag_member.java
+++ app/src/main/java/com/example/igo/main/Frag_member.java
@@ -12,15 +12,22 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.igo.R;
+import com.example.igo.login.LoginActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Frag_member extends Fragment {
 
     private View view;
+    private FirebaseAuth firebaseAuth;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.frag_member, container, false);
+        firebaseAuth = FirebaseAuth.getInstance();
+
         Button editButton = (Button)view.findViewById(R.id.edit_info);
         editButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,6 +36,18 @@ public class Frag_member extends Fragment {
                 view.getContext().startActivity(intent);
             }
         });
+
+        Button logoutButton = (Button)view.findViewById(R.id.logoutBtn);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                firebaseAuth.signOut();
+                Intent intent = new Intent(view.getContext(), LoginActivity.class);
+                view.getContext().startActivity(intent);
+            }
+        });
+
         return view;
     }
+
 }
